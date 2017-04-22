1cbb0ca256355839df68b30672cb978c8d7bbc03
XL <ayungan.fis@unach.edu.ec>
2017-04-21 20:27:10 -0500

    mi actualizacion24

--- app/src/main/java/com/truiton/rtmpplayer/MainActivity.java
+++ app/src/main/java/com/truiton/rtmpplayer/MainActivity.java
@@ -1,18 +1,20 @@
 package com.truiton.rtmpplayer;
-
-import android.os.Bundle;
 import com.example.angelo.app_video1.R;
-import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.MediaController;
-import android.widget.VideoView;
+
+import java.util.HashMap;
 
 import io.vov.vitamio.LibsChecker;
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.widget.MediaController;
+import io.vov.vitamio.widget.VideoView;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
     private String path;
     //private HashMap<String, String> options;
     private VideoView mVideoView;
@@ -38,48 +40,11 @@ public class MainActivity extends AppCompatActivity {
         mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
             public void onPrepared(MediaPlayer mediaPlayer) {
+
                 mediaPlayer.setPlaybackSpeed(1.0f);
             }
         });
-
-        mVideoView = (VideoView) findViewById(R.id.vitamio_videoView);
-        path = "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov";
-        mVideoView.setVideoPath(path);
-        mVideoView.setMediaController(new MediaController(this));
-        mVideoView.requestFocus();
-
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.setPlaybackSpeed(1.0f);
-            }
-        });
-
-        mVideoView = (VideoView) findViewById(R.id.vitamio_videoView);
-        path = "http://93.184.221.133/00573D/236/236-0.m3u8";
-        mVideoView.setVideoPath(path);
-        mVideoView.setMediaController(new MediaController(this));
-        mVideoView.requestFocus();
-
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.setPlaybackSpeed(1.0f);
-            }
-        });
-
-        mVideoView = (VideoView) findViewById(R.id.vitamio_videoView);
-        path = "mms://beotelmedia.beotel.net/studiob";
-        mVideoView.setVideoPath(path);
-        mVideoView.setMediaController(new MediaController(this));
-        mVideoView.requestFocus();
-
-        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.setPlaybackSpeed(1.0f);
-            }
-        });
-
     }
+
+
 }
